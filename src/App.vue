ea32d9c98e4446146ae823a6273f07dd9536c7d2
coder-ms <marco.sbrascia@gmail.com>
2022-11-16 00:10:37 +0200

    sistemazione app.vue

--- src/App.vue
+++ src/App.vue
@@ -20,39 +20,78 @@ import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppSearch from './components/AppSearch.vue';
 import CharacterList from './components/CharacterList.vue';
+import { store } from './store';
 
 // SCRIPT EXPORT DEFAULT
 export default {
   components: {
     AppHeader,
     AppSearch,
-    CharacterList
+    CharacterList,
   },
   data() {
     return {
-      apiURL: 'https://www.breakingbadapi.com/api/characters',
-      characterList: [],
-      loading: false,
-      searchType: ''
+      store,
+      endPoint: 'character',
     }
   },
+
   methods: {
     getCharacters() {
-      this.loading = true;
-      axios.get(this.apiURL).then(
+      store.errormessage = '';
+      let options = null;
+
+      if (store.search.status && store.search.name) {
+        options = {
+          params: {
+            status: store.search.status,
+            name: store.search.name
+          }
+        }
+      } else if (store.search.status) {
+        options = {
+          params: {
+            status: store.search.status
+          }
+        }
+      } else if (store.search.name) {
+        options = {
+          params: {
+            name: store.search.name
+          }
+        }
+      }
+      store.loading = true;
+
+      //
+      const apiurl = store.apiURL; //  + this.endPoint;
+
+      //
+      if ((null != options))
+        if ((null != options.params)) {
+          console.log('options.params.name = ' + options.params.name);
+          console.log('options.params.status = ' + options.params.status);
+        }
+      console.log('endPoint = ' + this.endPoint);
+      console.log('apiurl = ' + apiurl);
+
+      //
+      axios.get(apiurl, options).then(
         (res) => {
-          this.characterList = [...res.data];
-          console.log(this.characterList)
-          this.loading = false;
+          store.characterList = res.data;
+          store.loading = false;
         }
       ).catch((error) => {
-        this.loading = false;
-        console.log(error);
+        store.characterList.length = 0;
+        store.loading = false;
+        store.errormessage = error.message
+        // console.log(error.message);
+        // console.log(error.response.status);
       })
     }
   },
   created() {
-    this.getCharacters();
+    this.getCharacters()
   }
 }
 </script>
