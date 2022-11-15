ed255a568bbb65e6a4e06c9a596b97396e1e0b61
coder-ms <marco.sbrascia@gmail.com>
2022-11-15 14:40:46 +0200

    add form

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,6 +1,27 @@
 <template>
     <div class="container d-flex justify-content-center p-4">
-        Search bar
+        <form class="row row-cols-lg-auto g-3 align-items-center">
+            <div class="col-12">
+                <label>Search name</label>
+                <input type="text" class="form-control" placeholder="Search name">
+            </div>
+
+            <div class="col-12">
+                <label>Search status</label>
+                <select class="form-select" id="searchStatus">
+                    <option>Choose...</option>
+                    <option>Status</option>
+                </select>
+            </div>
+
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+
+            <div class="col-12">
+                <button type="reset" class="btn btn-primary">Reset</button>
+            </div>
+        </form>
     </div>
 
 </template>
